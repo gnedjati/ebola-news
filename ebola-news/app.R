4c42592a036da48ad99f3cfaa29bd15051d2b94a
gnedjati <g.nedjati-gilani@imperial.ac.uk>
2026-08-14 09:09:26 +0100

    thought i could specify a custom directory for the deployed site but i can't. oops

--- ebola-news/app.R
+++ ebola-news/app.R
@@ -20,6 +20,8 @@ library(tidytext)
 # data_dates = sapply(data_files, function(x) file.info(x)$mtime)
 # data_path = data_files[which.max(data_dates)]
 data_path = file.path("data","ebola-news_090826.csv")
+#case data
+case_path = file.path("data","cumulative_confirmed_cases__daily.csv")
 
 #load_data
 data <- read.csv(data_path)
@@ -34,6 +36,11 @@ data$Months = floor_date(data$Days, unit = "months")
 data$Months_select = format(data$Months, format = "%B %Y")
 # make another column by week
 data$Weeks = floor_date(data$Days, unit = "weeks")
+
+# load case data
+case_data <- read.csv(case_path)
+# make sure data is in correct date format
+case_data$Days = as.Date(case_data$date, tryFormats = c("%Y-%m-%d", "%d/%m/%Y"))
 
 
 # Define UI for news application with 
@@ -80,6 +87,7 @@ ui <- page_navbar(
                            sidebar = sidebar(open = "always",
                                              shiny::p(tags$h6("Explore articles over time")),
                                              uiOutput("select_timescale"),
+                                             checkboxInput("casedata", "Show case data", value = FALSE, width = NULL),
                                              uiOutput("colour_by"),
                                              conditionalPanel(
                                                condition = "input.colour_by != 'None'",
@@ -186,6 +194,7 @@ server <- function(input, output) {
   
   colour <- reactive({input$colour_by})
   stack <- reactive({input$stack})
+  casedata <- reactive({input$casedata})
   
   plot_dat <- reactive({
     df <- data %>%
@@ -198,6 +207,8 @@ server <- function(input, output) {
   
   output$plotTrends <- renderPlot({
     req(input$timescale)
+    
+    
     if('colour_by'%in%names(input) && input$colour_by != "None"){
       if(input$colour_by == "Country"){
         a = interaction(plot_dat()$Country, plot_dat()$Country2, plot_dat()$Country3, sep = " ")
@@ -215,10 +226,11 @@ server <- function(input, output) {
     }
     
     if('stack'%in%names(input) && input$stack){
-      g <- g + geom_bar()
+      g <- g + geom_bar() + ylab("Number of articles")
     } else{
-      g <- g + geom_bar(position = position_dodge(preserve = "single")) 
+      g <- g + geom_bar(position = position_dodge(preserve = "single")) + ylab("Number of articles")
     }
+
     
     #make subtitle
     if(is.null(input$publication)){
@@ -255,7 +267,20 @@ server <- function(input, output) {
     }
     subtitle = paste(pubstring, datestring, topicstring, countrystring)
     
-    g <- g + theme_bw(base_size = 15) + ylab("Number of articles") + labs(title = "Articles over time", subtitle = subtitle)
+    g <- g + theme_bw(base_size = 15) + labs(title = "Articles over time", subtitle = subtitle)
+    
+    if('casedata' %in% names(input) && input$casedata){
+      if(input$timescale == "Days") {
+        coeff = 200
+      } else if (input$timescale == "Weeks") {
+        coeff = 50
+      } else {
+        coeff = 30
+      }
+      g <- g + geom_line(data = case_data, aes(x = Days, y = national_cumulative_confirmed_cases/coeff, color = "black")) +
+        scale_y_continuous(sec.axis = sec_axis(~.*coeff, name = "Cumulative confirmed cases"))
+    }
+    
     g
   })
   
